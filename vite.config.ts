b7cd511b45b40d75dd8e56e2d110f05f2a6b1e57
gelodosertao <gelodosertaobahia@gmail.com>
2026-04-11 16:44:37 -0300

    fix: adicionar definições de tipo do vite e atualizar tsconfig

---
+++ vite.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [react()],
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './'),
+        },
+    },
+    build: {
+        outDir: 'dist',
+        sourcemap: true,
+    },
+    server: {
+        host: true,
+        port: 5173,
+    }
+});
